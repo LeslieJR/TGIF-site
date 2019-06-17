11f766ac8f5fb268c637913f91dc00b65d553013
Leslie Reyes <l.reyes.ubiqum@gmail.com>
2019-06-17 15:33:13 +0200

    data fetch and loader

--- TGIF/scripts/main.js
+++ TGIF/scripts/main.js
@@ -1,32 +1,66 @@
-var members = data.results[0].members;
-//the table is already created in the HTML file also the thead and the tboy, that last with the id="senate-data"
 
-fillUpTable(members);
-createArrayStates(members);
+var members;
+
+
+fetchingSenateHouse();
+function fetchingSenateHouse(){
+let link;
+let houseLink="https://api.propublica.org/congress/v1/113/house/members.json"
+let senateLink="https://api.propublica.org/congress/v1/113/senate/members.json"
+
+if (window.location.pathname.includes('senate')){
+    link = senateLink;
+} else if(window.location.pathname.includes('house')){
+    link= houseLink;
+}
+
+fetch(link, 
+{method:"GET",
+headers:
+{'X-API-Key':'wyfRhSLA71qOzm5StT9fMZ4nJE1GxARXIa4gKmSj'}
+}).then(function(response) {
+    if (response.ok){
+      return response.json();
+    }
+    throw new Error(response.status);
+  }).then(function (json){
+     members=json.results[0].members;
+     fillUpTable(members);
+     createArrayStates(members);
+     init();
+     document.getElementById('loader').style.display="none";
+     document.getElementById('pageContent').style.display="block";
+    }).catch(function(error){
+      console.log("Request failed:" + error.message);
+      });
+  
+    }
+
 
 function fillUpTable(arrayMembers) {
-  var tbody = document.getElementById("data");
+  let tbody = document.getElementById("data");
   tbody.innerHTML = '';
-  for (var i = 0; i < arrayMembers.length; i++) {
+  if(arrayMembers.length !==0){
+  for (let i = 0; i < arrayMembers.length; i++) {
     //creates a table row
-    var tr = document.createElement("tr");
+    let tr = document.createElement("tr");
 
-    var firstName = arrayMembers[i].first_name;
-    var middleName = arrayMembers[i].middle_name;
-    var lastName = arrayMembers[i].last_name;
+    let firstName = arrayMembers[i].first_name;
+    let middleName = arrayMembers[i].middle_name;
+    let lastName = arrayMembers[i].last_name;
 
-    var politicParty = arrayMembers[i].party;
-    var state = arrayMembers[i].state;
-    var seniority = arrayMembers[i].seniority;
-    var votesPercentage = arrayMembers[i].votes_with_party_pct;
-    var urlMember = arrayMembers[i].url;
+    let politicParty = arrayMembers[i].party;
+    let state = arrayMembers[i].state;
+    let seniority = arrayMembers[i].seniority;
+    let votesPercentage = arrayMembers[i].votes_with_party_pct;
+    let urlMember = arrayMembers[i].url;
 
     //creates cells inside a row (tr.append(td))
-    var td1 = document.createElement("td");
-    var td2 = document.createElement("td");
-    var td3 = document.createElement("td");
-    var td4 = document.createElement("td");
-    var td5 = document.createElement("td");
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
+    let td4 = document.createElement("td");
+    let td5 = document.createElement("td");
 
 
     if (middleName == null) {
@@ -46,17 +80,30 @@ function fillUpTable(arrayMembers) {
     tbody.append(tr);
   }
 }
+  else{
+    let tBody = document.getElementById("data");
+    tBody.innerHTML = '';
+    //creates a table row
+    let trow = document.createElement("tr");
+    let tdata = document.createElement("td");
+    tdata.colSpan = '5';
+    tdata.innerHTML = 'No match found';
+    tdata.style.textAlign = "center";
+    trow.append(tdata);
+    tBody.append(trow);
+  }
+}
 
 //we create a function called createArrayStates to print the options in the droplist (Select)
 function createArrayStates(array) {
-  var arrayState = []
-  for (var i = 0; i < array.length; i++) {
+  let arrayState = []
+  for (let i = 0; i < array.length; i++) {
     arrayState.push(array[i].state)
   }
 
   // after that we declare a new var in this case a set with the states (doing this we avoid repetitions) 
-  var set = new Set(arrayState);
-  var arrayOnlyStates = Array.from(set);
+  let set = new Set(arrayState);
+  let arrayOnlyStates = Array.from(set);
 
   //and then we sort that array alphabetically.
   arrayOnlyStates.sort()
@@ -65,7 +112,7 @@ function createArrayStates(array) {
   arrayOnlyStates.unshift('All');
 
   //we create a variable called droplist and it is going to be the selected element by id
-  var droplist = document.getElementById('sel1');
+  let droplist = document.getElementById('sel1');
 
   // here we use the .forEach to add new Options to the Select that has the state's name and also a value with the name of the state
   arrayOnlyStates.forEach(function (element, key) {
@@ -74,36 +121,39 @@ function createArrayStates(array) {
 }
 
 
-//here we add the eventListener to the checkboxes, so when it is clicked, a function is called (in this case, the function is called "filterByParty")
-document.querySelector('#defaultInline1').addEventListener('click', function () {
-  filterByParty(members)
-});
-document.querySelector('#defaultInline2').addEventListener('click', function () {
-  filterByParty(members)
-});
-document.querySelector('#defaultInline3').addEventListener('click', function () {
-  filterByParty(members)
-});
-//adding an eventlistener to the parent element (in this case the Select)
-document.querySelector('#sel1').addEventListener('change', function () {
-  filterByParty(members)
-})
+function init() {
+  //here we add the eventListener to the checkboxes, so when it is clicked, a function is called (in this case, the function is called "filterByParty")
+  document.querySelector('#defaultInline1').addEventListener('click', function () {
+    filterByParty(members)
+  });
+  document.querySelector('#defaultInline2').addEventListener('click', function () {
+    filterByParty(members)
+  });
+  document.querySelector('#defaultInline3').addEventListener('click', function () {
+    filterByParty(members)
+  });
+  //adding an eventlistener to the parent element (in this case the Select)
+  document.querySelector('#sel1').addEventListener('change', function () {
+    filterByParty(members)
+  })
+}
 
 
 //here we create the function filterByParty 
 function filterByParty(members) {
-  var inputs = document.getElementsByTagName("input");
-  var arrChecked = [];
-  var partyArrayChecked = [];
-  for (var i = 0; i < inputs.length; i++) {
+  let inputs = document.getElementsByTagName("input");
+  let arrChecked = [];
+  let partyArrayChecked = [];
+  for (let i = 0; i < inputs.length; i++) {
     if (inputs[i].type === "checkbox" && inputs[i].checked) {
       arrChecked.push(inputs[i].defaultValue)
     }
   }
   if (arrChecked.length !== 0) {
-    for (var k = 0; k < members.length; k++) {
-      for (var j = 0; j < arrChecked.length; j++) {
+    for (let k = 0; k < members.length; k++) {
+      for (let j = 0; j < arrChecked.length; j++) {
         if (members[k].party === arrChecked[j]) {
+          console.log(arrChecked[j])
           partyArrayChecked.push(members[k]);
         }
       }
@@ -116,16 +166,17 @@ function filterByParty(members) {
 
 //here we create the function filterByState
 function filterByState(members) {
-  var droplist = document.getElementById('sel1');
-  if (droplist.value === 'All') {
+  let droplist = document.getElementById('sel1');
+  if (droplist.value === 'All' && members.length !==0) {
     fillUpTable(members);
   } else {
-    var stateChecked = [];
-    for (var k = 0; k < members.length; k++) {
+    let stateChecked = [];
+    for (let k = 0; k < members.length; k++) {
+      
       if (members[k].state === droplist.value) {
         stateChecked.push(members[k]);
       }
     }
-    fillUpTable(stateChecked);
+      fillUpTable(stateChecked);
   }
 }
--- TGIF/scripts/statistics.js
+++ TGIF/scripts/statistics.js
@@ -1,41 +1,4 @@
-var members = data.results[0].members;
-
-
-
-function countMembers(arrayMembers, letter) {
-    var numMemberParty = 0;
-    for (var i = 0; i < arrayMembers.length; i++) {
-        if (arrayMembers[i].party === letter) {
-            numMemberParty++;
-        }
-    }
-    return (numMemberParty);
-}
-
-
-function getArray(anArray, letter) {
-    var newArray = [];
-    for (var i = 0; i < anArray.length; i++) {
-        if (members[i].party === letter) {
-            newArray.push(members[i]);
-        }
-    }
-    var numArray = 0;
-    for (var j = 0; j < newArray.length; j++) {
-        numArray += newArray[j].votes_with_party_pct;
-    }
-    var averageOfVotes = 0;
-
-    if (newArray.length !== 0) {
-        averageOfVotes = (numArray / newArray.length);
-
-    } else {
-        averageOfVotes = 0;
-    }
-    return (averageOfVotes);
-}
-
-
+var members;
 const statistics = {
     democrats: {
         name: "Democrats",
@@ -60,6 +23,84 @@ const statistics = {
     }
 };
 
+fetchingSenateHouse();
+
+function fetchingSenateHouse(){
+let link;
+let houseLink="https://api.propublica.org/congress/v1/113/house/members.json"
+let senateLink="https://api.propublica.org/congress/v1/113/senate/members.json"
+
+if (window.location.pathname.includes('senate')){
+    link = senateLink;
+} else if(window.location.pathname.includes('house')){
+    link= houseLink;
+}
+
+fetch(link, 
+{method:"GET",
+headers:
+{'X-API-Key':'wyfRhSLA71qOzm5StT9fMZ4nJE1GxARXIa4gKmSj'}
+}).then(function(response) {
+    if (response.ok){
+      return response.json();
+    }
+    throw new Error(response.status);
+  }).then(function (json){     
+     members=json.results[0].members;
+     populateObject(members);
+     showDataGlance(statistics, "table-glance");
+     createMemberArray(members, "missed_votes_pct");
+     createMemberArray(members, "votes_with_party_pct");
+     document.getElementById('loader').style.display="none";
+     document.getElementById('pageContent').style.display="block";
+
+    }).catch(function(error){
+      console.log("Request failed:" + error.message);
+      });
+    }
+
+    
+
+//here we create an object called statistics, and the sections number and average are equal to 0
+
+
+    function countMembers(arrayMembers, letter) {
+        let numMemberParty = 0;
+        for (let i = 0; i < arrayMembers.length; i++) {
+            if (arrayMembers[i].party === letter) {
+                numMemberParty++;
+            }
+        }
+        return (numMemberParty);
+    }
+    //function called getArray, first we create an empty array that is going to be filled with the members of a party ('R','D','I') and also we create a variable numArray that collects the sum of the votes-with-party of the array previously filled
+    
+    function getArray(anArray, letter) {
+        let newArray = [];
+        for (let i = 0; i < anArray.length; i++) {
+            if (members[i].party === letter) {
+                newArray.push(members[i]);
+            }
+        }
+        let numArray = 0;
+        for (let j = 0; j < newArray.length; j++) {
+            numArray += newArray[j].votes_with_party_pct;
+        }
+        let averageOfVotes = 0;
+    //if the array that contains the members of party is filled (meaning that there are at least one member) we calculate the average of votes.
+        if (newArray.length !== 0) {
+            averageOfVotes = (numArray / newArray.length);
+    
+        } else {
+            averageOfVotes = 0;
+        }
+        return (averageOfVotes);
+    }
+
+
+function populateObject(members){
+//we then update the sections by calling the functions needed.
+
 statistics.democrats.number = countMembers(members, "D")
 statistics.republicans.number = countMembers(members, "R")
 statistics.independents.number = countMembers(members, "I")
@@ -69,43 +110,42 @@ statistics.republicans.average = getArray(members, "R")
 statistics.independents.average = getArray(members, "I")
 
 
-
 statistics.totals.number = statistics.democrats.number + statistics.republicans.number + statistics.independents.number;
 
 
-var totalPercent = 0;
-for (var i = 0; i < members.length; i++) {
+let totalPercent = 0;
+for (let i = 0; i < members.length; i++) {
     totalPercent += members[i].votes_with_party_pct;
 }
 statistics.totals.average = totalPercent / members.length;
 
+}
 
 
 
 function showDataGlance(object, tbodyid) {
     let tbody = document.getElementById(tbodyid);
-    for (var key in object) {
-        var row = tbody.insertRow(-1);
+    for (let key in object) {
+        let row = tbody.insertRow(-1);
 
-        var cell1 = row.insertCell(0);
-        var element1 = document.createElement("td");
+        let cell1 = row.insertCell(0);
+        let element1 = document.createElement("td");
         element1.innerHTML = (object[key].name);
         cell1.append(element1);
 
-        var cell2 = row.insertCell(1);
-        var element2 = document.createElement("td");
+        let cell2 = row.insertCell(1);
+        let element2 = document.createElement("td");
         element2.innerHTML = (object[key].number)
         cell2.append(element2);
 
-        var cell3 = row.insertCell(2);
-        var element3 = document.createElement("td");
+        let cell3 = row.insertCell(2);
+        let element3 = document.createElement("td");
         element3.innerHTML = (object[key].average).toFixed(2)
         cell3.append(element3);
 
     }
 }
 
-showDataGlance(statistics, "table-glance");
 
 
 //function to create an array that also is sorted in descending order by a field/criteria, that after its top 10% is cut and also the bottom 10% (when the array is reverse) 
@@ -116,9 +156,9 @@ function createMemberArray(anArray, criteria) {
         return b[criteria] - a[criteria];
     })
 
-    var topArray = anArray.slice(0, Math.round((anArray.length) * 0.1));
+    let topArray = anArray.slice(0, Math.round((anArray.length) * 0.1));
 
-    for (var i = (Math.round((anArray.length) * 0.1)); i < anArray.length; i++) {
+    for (let i = (Math.round((anArray.length) * 0.1)); i < anArray.length; i++) {
         if (topArray[(topArray.length) - 1][criteria] === anArray[i][criteria]) {
             topArray.push(anArray[i])
 
@@ -128,12 +168,12 @@ function createMemberArray(anArray, criteria) {
     }
 
     // if we need to cut the bottom of that array, we reverse the array and then use the slice method 
-    var arrayReversed = anArray.reverse();
-    var bottomArray = arrayReversed.slice(0, Math.round((anArray.length) * 0.1));
+    let arrayReversed = anArray.reverse();
+    let bottomArray = arrayReversed.slice(0, Math.round((anArray.length) * 0.1));
 
-    for (var i = (Math.round((anArray.length) * 0.1)); i < anArray.length; i++) {
-        if (bottomArray[(bottomArray.length) - 1][criteria] === anArray[i][criteria]) {
-            bottomArray.push(anArray[i]);
+    for (let j = (Math.round((anArray.length) * 0.1)); j < anArray.length; j++) {
+        if (bottomArray[(bottomArray.length) - 1][criteria] === anArray[j][criteria]) {
+            bottomArray.push(anArray[j]);
         } else {
             break;
         }
@@ -152,15 +192,15 @@ function createMemberArray(anArray, criteria) {
 
 // To fill the tables we create a function with a for loop
 function printTables(membersArray, tbodyId, criteria1, criteria2) {
-    var tbody = document.getElementById(tbodyId);
-    for (var i = 0; i < membersArray.length; i++) {
-        var row = tbody.insertRow(-1);
-        var cell1 = row.insertCell(0);
-        var element1 = document.createElement("td");
-        var firstName = membersArray[i].first_name;
-        var middleName = membersArray[i].middle_name;
-        var lastName = membersArray[i].last_name;
-        var urlMember = membersArray[i].url;
+    let tbody = document.getElementById(tbodyId);
+    for (let i = 0; i < membersArray.length; i++) {
+        let row = tbody.insertRow(-1);
+        let cell1 = row.insertCell(0);
+        let element1 = document.createElement("td");
+        let firstName = membersArray[i].first_name;
+        let middleName = membersArray[i].middle_name;
+        let lastName = membersArray[i].last_name;
+        let urlMember = membersArray[i].url;
         if (middleName == null) {
             var fullName = `<a href="${urlMember}" target="_blank">${firstName} ${lastName}</a>`;
         } else {
@@ -169,18 +209,16 @@ function printTables(membersArray, tbodyId, criteria1, criteria2) {
         element1.innerHTML = fullName;
         cell1.append(element1);
 
-        var cell2 = row.insertCell(1);
-        var element2 = document.createElement("td");
+        let cell2 = row.insertCell(1);
+        let element2 = document.createElement("td");
         element2.innerHTML = membersArray[i][criteria1];
         cell2.append(element2);
 
-        var cell3 = row.insertCell(2);
-        var element3 = document.createElement("td");
+        let cell3 = row.insertCell(2);
+        let element3 = document.createElement("td");
         element3.innerHTML = membersArray[i][criteria2];
         cell3.append(element3);
 
     }
 }
 
-createMemberArray(members, "missed_votes_pct");
-createMemberArray(members, "votes_with_party_pct");
